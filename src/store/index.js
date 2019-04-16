70ec2ee2c87cfb10734f39d141aebb6560526682
jnarowski <jnarowski@gmail.com>
2019-04-16 12:59:53 -0600

    fixed issue with dispatch

--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,11 @@ import {
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-const configureStore = (initialState = {}) => {
-	return createStore(
-		reducers,
-		applyMiddleware(thunk)
-	);
-}
+const store = createStore(
+	reducers,
+	applyMiddleware(thunk)
+);
 
-const store = configureStore({})
 const dispatch = store.dispatch
 
 export default store
